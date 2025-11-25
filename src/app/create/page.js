528a17efa4e618c983fa6e2a5a90249baa053ba9
Denys <githubdenis9@gmail.com>
2025-11-24 16:25:22 -0800

    changes

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,10 +1,24 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Create() {
   const router = useRouter();
 
+  const [postTitleValue, setPostTitleValue] = useState("");
+
+  const [postDescriptionValue, setPostDescriptionValue] = useState("");
+
+  const handleTitleChange = (e) => {
+    setPostTitleValue(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setPostDescriptionValue(e.target.value);
+  };
+
+  console.log({ postTitleValue });
   return (
     <div className="createPost">
       <div className="postCard">
@@ -15,12 +29,16 @@ export default function Create() {
           type="text"
           className="createPostTitleInput"
           placeholder="Enter post title"
+          value={postTitleValue}
+          onChange={handleTitleChange}
         ></input>
 
         <p className="createPostDescription">Description</p>
         <textarea
           className="createPostDescriptionTextarea"
           placeholder="Enter post description"
+          value={postDescriptionValue}
+          onChange={handleDescriptionChange}
         ></textarea>
 
         <div className="createPostButtons">
